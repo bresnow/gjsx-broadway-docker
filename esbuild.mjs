0d7df6782b8230b5f8352d6aeafeced88acb31a6
Bresnow <bresnow@me.com>
2022-12-01 23:07:35 +0000

    Commit

--- esbuild.mjs
+++ esbuild.mjs
@@ -5,7 +5,15 @@ import { format } from "prettier"
 import Docker from "dockerode"
 let { red, green, blue, yellow } = chalk;
 const docker = new Docker({ socketPath: "/var/run/docker.sock" })
-
+async function updateService() {
+  (await docker.listServices()).forEach(ser => {
+    console.log(ser.ID)
+    console.log(ser.inspect())
+    // ser.update({}, () => {
+    //   console.log(ser.UpdateStatus)
+    // })
+  })
+}
 // --watch option
 let watch = argv.watch !== undefined;
 let entryPoints = await glob("{src,lib}/**/*.{ts,tsx}");
@@ -25,23 +33,22 @@ if (watch) {
         console.log(
           green(`Compiling ${blue(path)} after ${yellow(e.toUpperCase())} event`)
         );
-        (await docker.listServices()).forEach(ser => {
-          console.log(ser)
-        })
+        compile(path)
       } catch (error) {
         console.error(red(error.message))
 
       }
     });
   });
+  await updateService()
 } else {
   entryPoints.forEach((path) => {
     compile(path);
   });
+  await updateService()
 }
 
 function compile(_path) {
-
   let [dirRoute, ext] = _path.split(".");
   let readable = fs.createReadStream(_path, "utf8");
   let pathto = dirRoute.split("/"), basename = pathto[pathto.length - 1];
@@ -79,7 +86,7 @@ function compile(_path) {
         line = line.replace(/(gjsx\/utils)/, dotsToLibFromSrc + "/lib/gjsx/utils/index.js");
       };
       if (/(import)(.*)(from)(\s+)(("|')gjsx-ui("|'))/g.test(line)) {
-        line = line.replace(/(gjsx-ui)/, dotsToLibFromSrc + "/lib/gjsx/index.js");
+        line.replace(/(gjsx-ui)/, dotsToLibFromSrc + "/lib/gjsx/index.js");
       }
       return line
     });
